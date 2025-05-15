93fed71479e4d7c3de1b918a0092941fc6d765e1
Mustafa Saifee <mustafa.saifee@undp.org>
2025-05-15 15:06:52 +0300

    change the type for the value in the indicator files

--- scripts/script.py
+++ scripts/script.py
@@ -2,24 +2,27 @@ from pathlib import Path
 import json
 
 # Define the folder path
-folder = Path("../regionData")
+folder = Path("../indicatorData")
 
 # Get all JSON files
 json_files = folder.glob("*.json")
 
 # Loop through and modify each file
 for file in json_files:
-    with open(file, "r", encoding="utf-8") as f:
-        data = json.load(f)
-    print 
-    for indicator in data["indicators"]:
-        for val in indicator['yearlyData']:
-            val['year'] = int(val['year'])
-            try:
-                val['value'] = float(val['value'])
-                if val['value'].is_integer():
-                    val['value'] = int(val['value'])
-            except ValueError:
-                print(val['value'])            
-    with open(file, "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=2)
+    try:
+        with open(file, "r", encoding="utf-8") as f:
+            data = json.load(f)
+        print 
+        for countryData in data["countryData"]:
+            for val in countryData['data']:
+                val['year'] = int(val['year'])
+                try:
+                    val['value'] = float(val['value'])
+                    if val['value'].is_integer():
+                        val['value'] = int(val['value'])
+                except ValueError:
+                    print(val['value'])            
+        with open(file, "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=2)
+    except:
+        print(file)
